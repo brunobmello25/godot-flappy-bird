102c9bb9f0f7e0474a0a7d886c388e009f4a74e5
Bruno Mello <bruno.barros.mello@gmail.com>
2024-05-21 13:03:27 -0300

    character jumping

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,32 @@
 extends CharacterBody2D
 
-var gravity = 9.6
-var terminal_velocity = 20
+var gravity = 10
+var terminal_velocity = 100
+var jump_force = 300
+
+@onready var sprite = $AnimatedSprite2D
 
 func _ready():
 	pass
 
-func _process(delta):
-	velocity.y += gravity
+func _process(_delta: float):
+	handle_jump()
+	update_rotation()
+	accelerate()
 	move_and_slide()
+
+func accelerate():
+	velocity.y += gravity
+	#velocity.y = min(velocity.y + gravity, terminal_velocity)
+
+func handle_jump() -> void:
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+
+func update_rotation() -> void:
+	if velocity.y < -5:
+		sprite.rotation_degrees = 315
+	elif velocity.y > 5:
+		sprite.rotation_degrees = 45
+	else:
+		sprite.rotation_degrees = 0
