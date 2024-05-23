2487486da139a02a6ba68f17506528c8db606c9c
Bruno Mello <bruno.barros.mello@gmail.com>
2024-05-23 20:16:38 -0300

    setup pipe spawning and exit game

--- scenes/pipe_manager/pipe_manager.gd
+++ scenes/pipe_manager/pipe_manager.gd
@@ -6,26 +6,52 @@ extends Node2D
 
 @export var pipe_scene: PackedScene
 
-var min_top_height = -80;
-var max_top_height = 100;
-var speed = 120;
+var min_offset = -120
+var max_offset = 120
+var gap = 20
+var speed = 120
+
 
 func _ready():
 	pass
 
+
 func _process(delta: float):
 	move_pipes(delta)
 
+
 func spawn_pipes() -> void:
-	var top_pipe = pipe_scene.instantiate()
-	top_pipe.position.x = top_placement.position.x
-	top_pipe.position.y = max_top_height
-	top_pipe.rotation_degrees = 180
+	var offset = get_random_offset()
+	var top_pipe = _build_top_pipe(offset)
+	var bottom_pipe = _build_bottom_pipe(offset)
+
 	pipes_container.add_child(top_pipe)
+	pipes_container.add_child(bottom_pipe)
+
+
+func _build_top_pipe(offset: float):
+	var top_pipe = pipe_scene.instantiate()
+	top_pipe.position = top_placement.position
+	top_pipe.position.y += offset
+	top_pipe.rotation_degrees = 180
+	return top_pipe
+
+
+func _build_bottom_pipe(offset: float):
+	var pipe = pipe_scene.instantiate()
+	pipe.position = bottom_placement.position
+	pipe.position.y += offset
+	return pipe
+
+
+func get_random_offset() -> float:
+	return lerp(min_offset, max_offset, randf())
+
 
 func move_pipes(delta: float) -> void:
 	for pipe in pipes_container.get_children():
 		pipe.position.x -= speed * delta
+
 
 func _on_timer_timeout():
 	spawn_pipes()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-var gravity = 10
+var gravity = 20
 var terminal_velocity = 100
 var jump_force = 300
 
 @onready var sprite = $AnimatedSprite2D
 
+
 func _ready():
 	pass
+
 
 func _process(_delta: float):
 	handle_jump()
@@ -15,13 +17,16 @@ func _process(_delta: float):
 	accelerate()
 	move_and_slide()
 
+
 func accelerate():
 	velocity.y += gravity
 	#velocity.y = min(velocity.y + gravity, terminal_velocity)
 
+
 func handle_jump() -> void:
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_force
+
 
 func update_rotation() -> void:
 	if velocity.y < -5:
